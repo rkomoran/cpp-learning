5ce149bcb3ade7f07d07c4b7802239934a33340f
Rinor Komorani <komoranirinor@gmail.com>
2024-08-18 20:09:19 -0300

    Variables

---
+++ DeclaringVariables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "rinor";
+    cout << "Hey, my name is " << name;
+
+    int a = 10, b = 11, c = 12; 
+    cout << endl << a + b + c;
+
+    return 0;
+}
