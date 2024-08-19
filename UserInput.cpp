59fe57812e9914aec58157d8875b341acdab58a7
Rinor Komorani <komoranirinor@gmail.com>
2024-08-19 11:55:04 -0300

    Added some info about getline()

--- UserInput.cpp
+++ UserInput.cpp
@@ -1,5 +1,7 @@
 // calculator
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
@@ -8,8 +10,16 @@ int main()
     std::cin >> x;
 
     std::cout << "Type your second number\n";
+    // cin takes up to the following whitespace
     std::cin >> y;
 
     std::cout << x << "+" << y << "=" << x + y << "\n";
+
+    // getline() can take the whole line if no delimiter is specified
+
+    string sentence;
+    cout << "Hello, please enter a fact about you!\n";
+    getline(cin, sentence, '-');
+    cout << "Wow, " << sentence << " is pretty cool!";
     return 0;
 }
